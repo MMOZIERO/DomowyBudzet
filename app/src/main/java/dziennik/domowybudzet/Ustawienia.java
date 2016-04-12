32af872a671bf4ef0c26312ef9ed000f6f0775cd
Michał <moziero@wi.zut.edu.pl>
2016-04-12 10:00:55 +0200

    - Wyłączony tymczasowo splashscreen - Dodane podstawowe aktywnosci - Dodane menu - ...

--- app/src/main/java/dziennik/domowybudzet/Ustawienia.java
+++ app/src/main/java/dziennik/domowybudzet/Ustawienia.java
@@ -1,18 +1,21 @@
 package dziennik.domowybudzet;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Ustawienia extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ustawienia);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
@@ -66,4 +69,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
